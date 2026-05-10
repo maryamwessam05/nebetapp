a6cebd666d593ec5adba1521392f3466e8b86a0a
maryamwessam05 <23-101015@students.eui.edu.eg>
2026-05-10 16:37:05 +0300

    initial

---
+++ src/pages/approutes.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './home';
+
+
+const AppRoutes = () => {
+    return ( 
+        <>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+
+            </Routes>
+        </BrowserRouter>
+        </>
+     );
+}
+ 
+export default AppRoutes;
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.css"
+
+const Home = () => {
+    return ( 
+        <>
+        <main>
+            <h1>jj</h1>
+            <p>jhlnlknhohl</p>
+
+        </main>
+        </>
+     );
+}
+ 
+export default Home;
